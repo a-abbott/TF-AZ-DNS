84c0009165905f4533f9bda1fab4b10dab6e1112
Alexander Abbott <git config --global user.email>
2021-10-22 22:09:13 +0100

    Add basic public and private DNS frameworks, and SOA record options. Untested yet.

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@
 
 #Set TF Versions and providers
     terraform {
-        required_version = ">= 1.0.0"
+        required_version = var.tf_required_version
         backend "azurerm" {
         }
         required_providers {
             azurerm = {
-                source  = "hashicorp/azurerm"
-                version = "=2.73.0"
+                source  = var.tf_az_provider_source
+                version = var.tf_az_provider_version
             }
         }
     }
@@ -20,29 +20,83 @@
 #Configure the Microsoft Azure Provider and subscription
     provider "azurerm" {
         features {}
-        subscription_id = "INSERTVARIABLEFORSUBID"
+        subscription_id = var.azure_subscription_id
     }
 
 #Configure Tags
     locals {
         static_tags = {
-            CreatedDate = timestamp()
+            CreatedDate  = timestamp()
             ModifiedDate = timestamp()
         }
         common_tags = var.common_tags
-        extra_tags = var.extra_tags
+        extra_tags  = var.extra_tags
     }
 
 #Create Resouce Group
     resource "azurerm_resource_group" "RG" {
-        name     = var.RG_name
-        location = var.Location
+        name     = "${var.deployment_name}_rg"
+        location = var.location
         lifecycle {
         ignore_changes = [
             tags["CreatedDate"]
         ]
         }
-        tags     = merge(local.static_tags,var.common_tags, var.extra_tags)
+        tags = merge(local.static_tags,var.common_tags, var.extra_tags)
     }
 
 #Create AZ DNS
+
+    resource "azurerm_dns_zone" "AZ_DNS_Zone" {
+        count = var.enable_private_dns ? 1 : 0
+        name                = var.domain
+        resource_group_name = "${var.deployment_name}_rg"
+        resource "soa_record" {
+            count = var.enable_custom_soa ? 1 : 0
+            for_each = var.soa_record
+            content {
+                email = soa_record.value["email"]
+                host_name = soa_record.value["host_name"]
+                expire_time = soa_record.value["expire_time"]
+                minimum_ttl = soa_record.value["minimum_ttl"]
+                refresh_time = soa_record.value["refresh_time"]
+                retry_time = soa_record.value["retry_time"]
+                serial_number = soa_record.value["serial_number"]
+                tags = soa_record.value["tags"]
+                ttl = soa_record.value["ttl"]
+            }
+        }
+        lifecycle {
+        ignore_changes = [
+            tags["CreatedDate"]
+        ]
+        }
+        tags = merge(local.static_tags,var.common_tags, var.extra_tags)
+    }
+
+    resource "azurerm_private_dns_zone" "AZ_DNS_Zone" {
+        count = var.enable_private_dns ? 0 : 1
+        name                = var.domain
+        resource_group_name = "${var.deployment_name}_rg"
+        resource "soa_record" {
+            count = var.enable_custom_soa ? 1 : 0
+            for_each = var.soa_record
+            content {
+                email = soa_record.value["email"]
+                host_name = soa_record.value["host_name"]
+                expire_time = soa_record.value["expire_time"]
+                minimum_ttl = soa_record.value["minimum_ttl"]
+                refresh_time = soa_record.value["refresh_time"]
+                retry_time = soa_record.value["retry_time"]
+                serial_number = soa_record.value["serial_number"]
+                ttl = soa_record.value["ttl"]
+            }
+        }
+        lifecycle {
+        ignore_changes = [
+            tags["CreatedDate"]
+        ]
+        }
+        tags = merge(local.static_tags,var.common_tags, var.extra_tags)
+    }
+    
--- variables.tf
+++ variables.tf
@@ -1,17 +1,49 @@
 #Creator : Alexander Abbott
 #Source  : https://github.com/a-abbott/TF-AZ-DNS
 
+# TF Values
+#   These are values for terraform to use in the deployment.
+
+    variable "tf_required_version" {
+        type = string
+        # The name to be used for this deployment. This will be used to name the following
+        #   Resource groups (Assets, and Terraform State)
+        #   Assets created within produced RG's
+        default = ">= 1.0.0"
+    }
+    variable "tf_az_provider_source" {
+        type = string
+        # The name to be used for this deployment. This will be used to name the following
+        #   Resource groups (Assets, and Terraform State)
+        #   Assets created within produced RG's
+        default = "hashicorp/azurerm"
+    }
+    variable "tf_az_provider_version" {
+        type = string
+        # The name to be used for this deployment. This will be used to name the following
+        #   Resource groups (Assets, and Terraform State)
+        #   Assets created within produced RG's
+        default = "=2.73.0"
+    }
+
 # Deployment Values
 #   These are values for common use in the deployment.
 
-    variable "Deployment_name" {
+    variable "azure_subscription_id" {
+        type = string
+        # The azure subscription id to be used for this deployment.
+        default = ""
+    }
+
+    variable "deployment_name" {
         type = string
         # The name to be used for this deployment. This will be used to name the following
         #   Resource groups (Assets, and Terraform State)
         #   Assets created within produced RG's
         default = "YOUR RG NAME"
     }
-    variable "Location" {
+
+    variable "location" {
         type = string
         # Where the RG should be created. All assets in this pipeline will be deployed here
         # Valid choices for your subscription can be found via "az account list-locations", using the AzureCLI.
@@ -20,7 +52,56 @@
 
 #AZ DNS Deployment Values
 #   These are values for use creating the AZ DNS zone
+    variable "domain" {
+        type = string
+        # The domain this DNS zone represents.
+        default = "UK South" 
+    }
+    variable "enable_private_dns" {
+        type = bool
+        # If set to true, enable private DNS"
+        default = false
+    }
+    variable "enable_custom_soa" {
+        type = bool
+        # If set to true, enable configuration for custom SOA"
+        default = false
+    }
 
+    # If "enable_custom_soa" set to true, complete the following variables
+        variable "soa_record" {
+            # Required : email, host_name
+            # Optional : expire_time, fqdn, minimum_ttl, refresh_time, retry_time, serial_number, ttl
+            type = set(object(
+                {
+                    email         = string
+                    host_name     = string
+                    expire_time   = number
+                    fqdn          = string
+                    minimum_ttl   = number
+                    refresh_time  = number
+                    retry_time    = number
+                    serial_number = number
+                    ttl           = number
+                }
+            ))
+            default = [
+                {
+                    email         = ""
+                    host_name     = ""
+                    expire_time   = ""
+                    fqdn          = ""
+                    minimum_ttl   = ""
+                    refresh_time  = ""
+                    retry_time    = ""
+                    serial_number = ""
+                    ttl           = ""
+                }
+            ]
+        }
+
+#AZ DNS Record Config Values
+#   These are values for use configuring records the AZ DNS zone
 
 #Deployment tags
 #   These are tags to attach to all deployed assets.
